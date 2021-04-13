5cf7b83b9a274a6a4b91a6ae054c83de0162688c
Evan Azemard <71875653+evan-azemard@users.noreply.github.com>
2021-04-13 20:31:30 +0200

    Ajout page produit 100% faite + lien d'accé

--- view/produit.php
+++ view/produit.php
@@ -1,29 +1,47 @@
-
-<section>
-    <h1>Découvrer nos smartphones</h1>
-</section>
-<section>
-    <aside>Liste des smartphone </aside>
-    <article class="articlegrid">
-        <?php
-        foreach ($articles_model as $tab){
-            $model = $tab['marque_model'];
-
-            $articles = article_produit($model);
-            ?>
-            <h1><?= $tab['marque_model'] ?></h1>
-            <div id="flexcard">
-                <div class="grid">
-                    <?php foreach ($articles as $article){?>
-                        <div class="child card">
-                            <h2><?= $article['nom_model']?></h2>
-                            <h3><?= $article['resum']?></h3>
-                            <a href="article?id=<?= $article['id_produit']?>">Voir l'article</a>
-                        </div>
-                    <?php } ?>
-                </div>
-            </div>
+<main id="produit_main">
+    <!--Affiche produit par section-->
+    <div class="produit_h1">
+        <h1>Découvrer nos smartphones</h1>
+    </div>
+    <section>
+        <article class="articlegrid">
             <?php
-        } ?>
-    </article>
-</section>
+            foreach ($articles_model as $tab){
+                $model = $tab['marque_model'];
+
+                $articles = article_produit($model);
+                ?>
+                <div class="produit_h1">
+                    <h1><?= $tab['marque_model'] ?></h1>
+                </div>
+                <div id="flexcard">
+                    <div class="grid">
+                        <?php foreach ($articles as $article){?>
+                            <article class="produit_card">
+                                <div class="card_img">
+                                    <img src="img_docs/exemple.png.jpg" alt="exemple">
+                                </div>
+                                <div class="card_description">
+                                    <h3><?= $article['nom_model']?></h3>
+                                    <p><?= $article['resum']?></p>
+                                </div>
+                                <div class="logo_card">
+                                    <i  class="fa fa-shopping-cart fa-3x" aria-hidden="true"></i>
+                                    <input type="number" class="number" id="number" placeholder="1" min="1" max="500"><label for="number"></label>
+                                        <a href="article?id=<?= $article['id_produit']?>"><i class="fa fa-info-circle fa-3x" aria-hidden="true"></i></a>
+                                        <p>700 €</p>
+                                </div>
+                            </article>
+                        <?php } ?>
+                    </div>
+                </div>
+                <div class="traimoyen"></div>
+                <?php
+            } ?>
+        </article>
+    </section>
+</main>
+
+
+
+
